772afd9de46f418810007614e8c135933451045f
Hristov Pavlin (RBBG/ENG2) <Pavlin.Hristov@bg.bosch.com>
2021-10-20 13:30:13 +0300

    finish rewind

--- js/site.js
+++ js/site.js
@@ -1,4 +1,27 @@
-// Here we go again...
-function HelloWorld(){
-    alert("Hello World!")
+// GET the string
+function getTheString(){
+    document.getElementById("alert").classList.add("invisible");
+    let urText = document.getElementById("userString").value;
+    let revText = reverseTheString(urText)
+    displayTheString(revText)
+}
+
+// Reverse the string
+function reverseTheString(urText){
+    if (userString.length > 1) {
+        let revString = [];
+        for (let index = urText.length - 1; index >= 0; index--) {
+            revString += urText[index];
+        }
+    }   
+    else {
+        alert("You must enter atleast 2 characters in order to reverse!")
+    }
+    return revString;
+}
+
+// Display the string
+function displayTheString(revText){
+    document.getElementById("msg").innerHTML = `Your reversed string is: ${revText}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
